451db163f0bebd564659b74265911492aa91f102
BLimaM4I <geral@m4i.pt>
2023-02-15 16:56:36 +0000

    OrderListItem

---
+++ src/screens/OrdersScreen/index.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const OrderScreen = () => {
+  return (
+    <View>
+      <Text>OrderScreen</Text>
+    </View>
+  );
+};
+
+export default OrderScreen;
+
+const styles = StyleSheet.create({});
